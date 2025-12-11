8f778da723f9764a97a152bf4b0cae14562fee1e
binishshha <binishabasnet299@gmail.com>
2025-12-11 21:51:41 +0545

    final-1

--- src/components/contactus/contact.jsx
+++ src/components/contactus/contact.jsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa6";
 import { FiPhoneCall } from "react-icons/fi";
 import  Contactform  from "./contactform.jsx";
 import './contactus.css';
-import  Footer  from "../footer";
+import  Footer  from "../Footer";
 
 export default function Contact() {
   return (
